631158aba4f1bba213c3b4786d4b8d8dffef2c49
Tomohiro Watanabe <watanabe@vantiq.com>
2022-08-04 11:11:31 +0900

    Change newline in tf file

--- vantiq-cloud-infra-operations/terraform_aws/env-template/main.tf
+++ vantiq-cloud-infra-operations/terraform_aws/env-template/main.tf
@@ -23,7 +23,7 @@ terraform {
   required_version = ">= 1.1.8"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">= 4.10.0"
     }
   }
