2bb97d1ec322639cc79b20c61f0e2cb08c417fee
Masanori Kono <61811996+MasanoriKono@users.noreply.github.com>
2023-10-03 00:36:57 +0900

    Update setting.py
    masking secrets and urls

--- vantiq-devices-integration/conf/vantiq-videostream-safie/setting.py
+++ vantiq-devices-integration/conf/vantiq-videostream-safie/setting.py
@@ -1,14 +1,14 @@
 # LiveStream
-safie_api_key = "35de026644abd01be730538192cafe82f800"
-safie_device_id = "6Ywr0GBVHZxTZrRMJLT0"
-camera_id = "One (SF-1)"
+safie_api_key = "<your_safie_api_key>   # ex. "35de026644abd01bxxxxxxxxxxcafe82f800"
+safie_device_id = "<your_safie_device_id>"  # ex. "6Ywr0GBVHZxxxxxxxxxx"
+camera_id = "<your_camera_id>"    # ex. "One (SF-1)"
 interval_sec = 2
 
 # VANTIQ Configurations
-ACCESS_TOKEN = "L-SXBttH1dFEQwmnGz6Z2YfWP0vopim9exEmxpagW_8="
-VANTIQ_TOPIC_URL = "https://dev.vantiq.co.jp/api/v1/resources/topics//jp.co.vantiq.ws/detected/object"
+ACCESS_TOKEN = "<your_vantiq_access_token>"
+VANTIQ_TOPIC_URL = "<your_vantiq_server>/api/v1/resources/topics//jp.co.vantiq.ws/detected/object"
 VANTIQ_UPLOAD_PATH = "public/detected/object/image/"
-VANTIQ_DOCUMENTS_URL = "https://dev.vantiq.co.jp/api/v1/resources/documents"
+VANTIQ_DOCUMENTS_URL = "<your_vantiq_server>/api/v1/resources/documents"
 
 # モデルの入力パラメーターを設定する
 scale = 1.0 / 255.0     # スケールファクター
