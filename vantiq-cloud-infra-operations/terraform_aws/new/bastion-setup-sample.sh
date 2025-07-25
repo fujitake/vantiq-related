1a13bb1dd35faa677691df564e128030bbf42843
takahashi <KTakahashi@vantiq.com>
2025-07-25 11:25:40 +0900

    Terraform code maintenance

--- vantiq-cloud-infra-operations/terraform_aws/new/bastion-setup-sample.sh
+++ vantiq-cloud-infra-operations/terraform_aws/new/bastion-setup-sample.sh
@@ -20,14 +20,24 @@ apt-get install -y docker-ce docker-ce-cli containerd.io
 # add user to docker group to make docker accessible without sudo
 gpasswd -a ubuntu docker
 
+# copy ssh-key
+cat <<EOF > /home/ubuntu/.ssh/id_rsa
+${worker_access_private_key}
+EOF
+EOF
+chmod 444 /home/ubuntu/.ssh/id_rsa
+
 # install java
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
-apt-add-repository "deb http://repos.azul.com/azure-only/zulu/apt stable main"
+apt-key adv \
+  --keyserver hkp://keyserver.ubuntu.com:80 \
+  --recv-keys 0xB1998361219BD9C9
+curl -O https://cdn.azul.com/zulu/bin/zulu-repo_1.0.0-3_all.deb
+apt-get install -y ./zulu-repo_1.0.0-3_all.deb
 apt-get -q update
-apt-get -y install zulu-11-azure-jdk
+apt-get install -y zulu${jdk_version}-jdk
 
 # install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.24.10/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/release/v${bastion_kubectl_version}/bin/linux/amd64/kubectl
 chmod +555 ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 
@@ -40,8 +50,8 @@ apt-get install helm
 ln -s /usr/sbin/helm /usr/sbin/helm3
 
 # install kubeseal
-#wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.9.5/kubeseal-linux-amd64 -O kubeseal
-#install -m 755 kubeseal /usr/local/bin/kubeseal
+# wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.9.5/kubeseal-linux-amd64 -O kubeseal
+# install -m 755 kubeseal /usr/local/bin/kubeseal
 
 # install chromium
 apt install -y chromium-browser
