9941bf8a15a4f27a5061a2ce3b625b2e29304da3
Morinosuke Yamaguchi <myamaguchi@vantiq.com>
2025-09-25 10:22:25 +0900

    apt-keyコマンド廃止に伴う踏み台サーバ初期設定スクリプトのコード変更

--- vantiq-cloud-infra-operations/terraform_aws/new/bastion-setup-sample.sh
+++ vantiq-cloud-infra-operations/terraform_aws/new/bastion-setup-sample.sh
@@ -11,21 +11,23 @@ apt-get -y install \
     software-properties-common
 
 # install docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # add user to docker group to make docker accessible without sudo
 gpasswd -a ubuntu docker
 
 # install java
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
-apt-add-repository "deb http://repos.azul.com/azure-only/zulu/apt stable main"
+curl -s https://repos.azul.com/azul-repo.key | sudo gpg --dearmor -o /usr/share/keyrings/azul.gpg
+echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list
 apt-get -q update
-apt-get -y install zulu-11-azure-jdk
+apt-get -y install zulu11-jdk
 
 # install kubectl
 curl -LO https://dl.k8s.io/release/v1.32.7/bin/linux/amd64/kubectl
@@ -33,11 +35,10 @@ chmod +555 ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 
 # install helm 3 latest
-curl https://helm.baltorepo.com/organization/signing.asc | apt-key add -
-apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 apt-get update
-apt-get install helm
+apt-get install -y helm
 ln -s /usr/sbin/helm /usr/sbin/helm3
 
 # install kubeseal
--- vantiq-cloud-infra-operations/terraform_azure/new/modules/opnode/init-script.sh
+++ vantiq-cloud-infra-operations/terraform_azure/new/modules/opnode/init-script.sh
@@ -5,15 +5,18 @@ apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
+    gnupg \
     gnupg-agent \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 # add user to docker group to make docker accessible without sudo
 gpasswd -a ${user_name} docker
 
@@ -25,13 +28,13 @@ EOF
 chmod 444 /home/${user_name}/.ssh/id_rsa
 
 # install java
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
-apt-add-repository "deb http://repos.azul.com/azure-only/zulu/apt stable main"
+curl -s https://repos.azul.com/azul-repo.key | sudo gpg --dearmor -o /usr/share/keyrings/azul.gpg
+echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list
 apt-get -q update
-apt-get -y install zulu-${jdk_version}-azure-jdk
+apt-get install -y zulu${jdk_version}-jdk
 
 # install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v${kubectl_version}/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/release/v${kubectl_version}/bin/linux/amd64/kubectl
 chmod +555 ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 
@@ -43,11 +46,10 @@ mv ./kubectl /usr/local/bin/kubectl
 #mv linux-amd64/tiller /usr/local/bin
 
 # install helm 3 latest
-curl https://helm.baltorepo.com/organization/signing.asc | apt-key add -
-apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | tee /etc/apt/sources.list.d/helm-stable-debian.list
+curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
 apt-get update
-apt-get install helm
+apt-get install -y helm
 ln -s /usr/sbin/helm /usr/sbin/helm3
 
 # install kubeseal
